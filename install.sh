6ad665bd4b7b72b31998478280a87127bcb4792d
Migalin Danila <miga@migalin.net>
2013-07-08 21:48:40 +0400

    Added chsh

--- install.sh
+++ install.sh
@@ -10,3 +10,11 @@ if [ $ans = 'y' -o $ans = 'Y' ] ; then
 	cp -r $dir/lib/ $dir/plugins/ $dir/themes/ $dir/oh-my-zsh.sh $HOME/.myzsh
 	cp $dir/zshrc $HOME/.zshrc
 fi 
+
+echo -n 'Do you want to change your shell to zsh now (y/n)?'
+read ans
+if [ $ans = 'y' -o $ans = 'Y' ] ; then
+    echo "Running chsh. It will ask you for you password."
+    chsh -s `which zsh`
+fi 
+
